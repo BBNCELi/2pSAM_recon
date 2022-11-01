64a7c735818084f822e6332a7b6e19ee22552802
Yiliang ZHOU <59339977+BBNCELi@users.noreply.github.com>
2022-11-01 12:05:16 +0800

    Delete code directory

--- code/utils/utils_common/ifftForImage.m
+++
@@ -1,7 +0,0 @@
-function output = ifftForImage(input)
-
-if ndims(input)~=2
-    error();
-end
-
-output = fftshift(ifft2(ifftshift(input)));
--- code/utils/utils_deconv/projectors/forwardProjectRLGPU_fourier.m
+++
@@ -1,6 +0,0 @@
-function HXguessFFT = forwardProjectRLGPU_fourier(largepsf,Xguess,psf_s)
-
-FFT3PSFFlip = fftForStack(flip(largepsf,3));
-HXguessFFT = sum( fftForStack(Xguess) .* FFT3PSFFlip ,3)./psf_s;
-
-end
